f7357377e0fae9632b6620fcd9105a2218fcbad6
MarinaShemesh <marina.shemesh1@gmail.com>
2017-03-13 11:05:17 +0200

    new rocket

--- rocket.js
+++ rocket.js
@@ -2,9 +2,13 @@
  * Created by karina on 13/03/17.
  */
 "use strict";
- var rocket = function (color, size) {
-   this.color = color;
-   this.size - size;
- }
+var Rocket = function (color, size) {
+  this.color = color;
+  this.size = size;
 
- module.exports = rocket;
+  this.launch = function () {
+    console.log('3, 2, 1, blastoff!');
+  }
+}
+
+module.exports = Rocket;
